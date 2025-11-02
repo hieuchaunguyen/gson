d1d63f32df6be9e27451d9c59146147776ad969c
Michel Assouka <michelassouka@gmail.com>
2025-11-02 07:37:20 -0500

    Use logger instead of System.out for output
    Replaced System.out.println and System.out.printf with logger for better logging practices.

--- extras/src/main/java/com/google/gson/extras/examples/rawcollections/RawCollectionsExample.java
+++ extras/src/main/java/com/google/gson/extras/examples/rawcollections/RawCollectionsExample.java
@@ -24,6 +24,9 @@ import java.util.logging.Logger;
 
 @SuppressWarnings({"PrivateConstructorForUtilityClass", "SystemOut"})
 public class RawCollectionsExample {
+
+  private static final Logger LOGGER = Logger.getLogger(RawCollectionsExample.class.getName());
+
   static class Event {
     private String name;
     private String source;
@@ -42,16 +45,23 @@ public class RawCollectionsExample {
   @SuppressWarnings({"unchecked", "rawtypes"})
   public static void main(String[] args) {
     Gson gson = new Gson();
+
     Collection collection = new ArrayList();
     collection.add("hello");
     collection.add(5);
     collection.add(new Event("GREETINGS", "guest"));
+
     String json = gson.toJson(collection);
-    LOGGER.info(("Using Gson.toJson() on a raw collection: " + json);
+
+    // Replaced System.out.println with logger
+    LOGGER.info("Using Gson.toJson() on a raw collection: " + json);
+
     JsonArray array = JsonParser.parseString(json).getAsJsonArray();
     String message = gson.fromJson(array.get(0), String.class);
     int number = gson.fromJson(array.get(1), int.class);
     Event event = gson.fromJson(array.get(2), Event.class);
-    System.out.printf("Using Gson.fromJson() to get: %s, %d, %s", message, number, event);
+
+    // Replaced System.out.printf with logger
+    LOGGER.info(String.format("Using Gson.fromJson() to get: %s, %d, %s", message, number, event));
   }
 }

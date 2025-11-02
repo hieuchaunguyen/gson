2e9948b14e043984b9ea785312e16ea5f01d7fca
Michel Assouka <michelassouka@gmail.com>
2025-11-02 08:26:49 -0500

    Update RawCollectionsExample by removing copyright and logging
    Removed copyright notice and replaced System.out.println with logger.

--- extras/src/main/java/com/google/gson/extras/examples/rawcollections/RawCollectionsExample.java
+++ extras/src/main/java/com/google/gson/extras/examples/rawcollections/RawCollectionsExample.java
@@ -1,18 +1,3 @@
-/*
- * Copyright (C) 2011 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.google.gson.extras.examples.rawcollections;
 
 import com.google.gson.Gson;
@@ -45,12 +30,10 @@ public class RawCollectionsExample {
   @SuppressWarnings({"unchecked", "rawtypes"})
   public static void main(String[] args) {
     Gson gson = new Gson();
-
     Collection collection = new ArrayList();
     collection.add("hello");
     collection.add(5);
     collection.add(new Event("GREETINGS", "guest"));
-
     String json = gson.toJson(collection);
 
     // Replaced System.out.println with logger
